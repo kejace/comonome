01542f49c9e846cf41085346e64458185ed823c9
Kristoffer Josefsson <kejace@gmail.com>
2015-03-19 19:08:25 -0400

    Made send and receive work with Monome.

--- receive.hs
+++ receive.hs
@@ -2,7 +2,6 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TemplateHaskell     #-}
 
-
 import Control.Concurrent
 import Control.Monad
 
@@ -17,8 +16,8 @@ type Handler = OSC.Message -> IO ()
 
 waitForOsc :: Handler -> IO ()
 waitForOsc handler = do
-  let port = 54321
-  putStrLn $ "Listening for OSC messages on port " ++ show 54321
+  let port = 15144
+  putStrLn $ "Listening for OSC messages on port " ++ show port
   let t = OSC.udpServer "127.0.0.1" port
   Sound.OSC.Transport.FD.withTransport t $ \t -> void $ OSC.untilPredicate not {-should be not-} $ do
     msgs <- Sound.OSC.Transport.FD.recvMessages t
@@ -30,11 +29,31 @@ waitForOsc handler = do
 isQuitMessage :: OSC.Message -> Bool
 isQuitMessage m = "/comonome/quit" `isPrefixOf` OSC.messageAddress m 
 
-statusHandler :: Handler
-statusHandler m = when ("/comonome/status" `isPrefixOf` OSC.messageAddress m) $
-  -- TODO print more info
-  putStrLn "Comomoe is getting it"
+monomeStringHandler :: Handler
+monomeStringHandler m = when ("/monome/grid/led/set" `isPrefixOf` OSC.messageAddress m) $ go m
+  where
+    go (OSC.Message _ [OSC.ASCII_String x, OSC.ASCII_String y, OSC.ASCII_String s])
+      = putStrLn $ "got strings: x,y,z = " ++ show x -- >> return ()
+    go _ = return ()
 
+monomeIntHandler :: Handler
+monomeIntHandler m = when ("/monome/grid/led/set" `isPrefixOf` OSC.messageAddress m) $ go m
+  where
+    go (OSC.Message _ [OSC.Int32 x, OSC.Int32 y, OSC.Int32 s])
+      = putStrLn $ "got ints: x,y,z = " ++ show s -- >> return () 
+    go _ = return ()     
+
+statusHandler :: Handler
+statusHandler m = when ("/comonome/status" `isPrefixOf` OSC.messageAddress m) $ go m
+  where go m = putStrLn $ "Comonome is getting it"
+
+composeHandlers :: [Handler] -> Handler
+composeHandlers = foldr composeHandlers2 (\x -> return ())
+
+composeHandlers2 :: Handler -> Handler -> Handler
+composeHandlers2 f g m = do
+  f m
+  g m  
 
 main :: IO ()
-main = waitForOsc (statusHandler)
+main = waitForOsc $ composeHandlers [monomeStringHandler, monomeIntHandler, statusHandler]
--- sender.hs
+++ sender.hs
@@ -2,27 +2,22 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TemplateHaskell     #-}
 
-
-import Control.Concurrent
 import Control.Monad
 
 import System.Posix.Unistd
 import System.Posix
 
-import           Data.ByteString.Char8            (ByteString (..))
 import qualified Data.ByteString.Char8            as BS
 
-import           Data.List                        (isPrefixOf)
 import qualified Sound.OSC                        as OSC
 import qualified Sound.OSC.Transport.FD
 
-type Handler = OSC.Message -> IO ()
 
-kOSCDest = ("127.0.0.1", 54321)
+kOSCDest = ("127.0.0.1", 15144)
 
 data Action
   = SendOSC
-      String [String] -- address port pattern args (i.e. "/fluent/play" ["n1","1"])
+      String [OSC.Datum] -- address port pattern args (i.e. "/fluent/play" ["n1","1"])
 
 sendMoreOSC :: Action -> IO ()
 sendMoreOSC = go
@@ -31,22 +26,11 @@ sendMoreOSC = go
       putStrLn "Sending some OSC"
       blockSignals $ addSignal sigVTALRM emptySignalSet
       sleep 2
-      let msg = OSC.Message patt (fmap (OSC.ASCII_String . BS.pack) args)
+      --let msg = OSC.Message patt (fmap (OSC.ASCII_String . BS.pack) args)
+      let msg = OSC.Message patt args
       oscOut <- let (addr, port) = kOSCDest in OSC.openUDP addr port
       -- putStrLn $ "Sending " ++ show msg
       Sound.OSC.Transport.FD.sendMessage oscOut $ msg
 
-sendSomeOsc :: IO ()
-sendSomeOsc = do
-
-  oscOut <- let (addr, port) = kOSCDest in OSC.openUDP addr port
-  let port = 54321
-  putStrLn $ "Sending some OSC on " ++ show port
-  let t = OSC.udpServer "127.0.0.1" port
-  let msg = OSC.Message "/comonome/status" (fmap (OSC.ASCII_String . BS.pack) ["hello"])
-  --Sound.OSC.Transport.FD.withTransport t $ \t -> void $ OSC.untilPredicate not {-should be not-} $ do
-  --  msgs <- Sound.OSC.Transport.FD.sendMessage msg t
-  return ()
-
 main :: IO ()
-main = sendMoreOSC $ SendOSC "/comonome/status" ["hello"]
+main = sendMoreOSC $ SendOSC "/monome/grid/led/set" [OSC.Int32 1, OSC.Int32 1, OSC.Int32 1]
